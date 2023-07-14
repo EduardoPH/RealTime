a3d2d372d59e370277eb13b27d043131ff76784a
dudu <dudu@dudus-iMac.local>
2023-07-13 22:08:30 -0300

    Adicionando dia

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,16 @@
 const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
+const dias = document.getElementById('dias');
 
 const relogio = setInterval(function time() {
   let dateToday = new Date();
   let hr = dateToday.getHours();
   let min = dateToday.getMinutes();
   let sec = dateToday.getSeconds();
+  let day = dateToday.getDate();
+
+  if(day < 10) day = '0' + day;
 
   if(hr < 10) hr = '0' + hr;
 
@@ -14,6 +18,7 @@ const relogio = setInterval(function time() {
 
   if(sec < 10) sec = '0' + sec;
 
+  dias.textContent = day
   horas.textContent = hr;
   minutos.textContent = min;
   segundos.textContent = sec;
